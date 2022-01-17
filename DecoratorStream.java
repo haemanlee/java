236c3ebb1e8bf54b6b37b7bbc8e58f8895e5076c
Haeman Lee <37089983+haemanlee@users.noreply.github.com>
2022-01-18 00:49:06 +0900

    testdorm java coding study

---
+++ DecoratorStream.java
@@ -0,0 +1,65 @@
+package hello.proxy.hyper;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+public class DecoratorStream extends OutputStream
+{
+    private OutputStream stream;
+    private String prefix;
+
+    public DecoratorStream(OutputStream stream, String prefix) {
+        super();
+        this.stream = stream;
+        this.prefix = prefix;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        byte[] result = new byte[4];
+
+        result[0] = (byte) (b >> 24);
+        result[1] = (byte) (b >> 16);
+        result[2] = (byte) (b >> 8);
+        result[3] = (byte) (b);
+
+        write(result, 0, 4);
+    }
+    private static boolean prefixused;
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        if(prefixused){
+            stream.write(b, 0, b.length);
+        } else {
+            for(char c : prefix.toCharArray()){
+                stream.write((int)c);
+            }
+            stream.write(b, 0, b.length);
+            prefixused = true;
+        }
+
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] message = new byte[]{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21};
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            DecoratorStream decoratorStream = new DecoratorStream(baos, "First line: ");
+            decoratorStream.write(message);
+
+            //다시 utf-8로 바꿔서 실행
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "UTF-8"))) {
+                System.out.println(reader.readLine());  //should print "First line: Hello, world!"
+            }
+        }
+    }
+}
---
+++ QuadraticEquation.java
@@ -0,0 +1,24 @@
+package hello.proxy.hyper;
+
+public class QuadraticEquation {
+    public static Roots findRoots(double a, double b, double c) {
+        double x1 = (-b + Math.sqrt(b*b - 4*a*c) ) / 2*a;
+        double x2 = (-b - Math.sqrt(b*b - 4*a*c) ) / 2*a;
+        Roots roots = new Roots(x1, x2);
+        return roots;
+    }
+
+    public static void main(String[] args) {
+        Roots roots = QuadraticEquation.findRoots(2, 10, 8);
+        System.out.println("Roots: " + roots.x1 + ", " + roots.x2);
+    }
+}
+
+class Roots {
+    public final double x1, x2;
+
+    public Roots(double x1, double x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+}
